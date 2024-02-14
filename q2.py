6ca153c8ed97f93c8d28c2d7869f4ea33bb3b96c
joshuadinham <joshua-dinham@hotmail.com>
2024-02-14 17:38:21 -0500

    copy python files

--- q2.py
+++ q2.py
@@ -1,1 +1,41 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sys
+
+filename = sys.argv[1]
+
 def graphSnowfall(t):
+    """read numbers from a file and graph the number of occurrences within each range as a bar graph """
+    numbers = []
+    with open(t) as numberFile:
+        for line in numberFile:
+            numbers.append(int(line.strip()))
+
+    print(numbers)
+    sorted_numbers = sorted(numbers)
+    print(sorted_numbers)
+    #create dictionary with ranges as the x value, and occurrences as y value
+    snow_fall_ranges = {"0-10cm":0, "11-20cm":0, "21-30cm":0, "31-40cm":0, "41-50cm":0, "51-60cm":0, "60+":0}
+    
+    snow_fall_keys = list(snow_fall_ranges)
+
+    #for each number in sorted numbers list
+        #find the index by using the floor function -1 to account for 11/21/31 ect
+        #increment the dictionary value at the key by using the index to find the key name from the list of keys
+    for number in sorted_numbers: 
+        index = (number - 1) // 10
+        snow_fall_ranges[snow_fall_keys[index]] += 1
+
+    print(snow_fall_ranges)
+
+    #plot the bar graph
+        #range is the amount of keys, names are the key values
+        #the ticks for x increment are the key of the dictionary as well
+    plt.bar(range(len(snow_fall_ranges)), snow_fall_ranges.values(), align='center')
+    plt.xticks(range(len(snow_fall_ranges)), list(snow_fall_ranges))
+    plt.show()
+
+graphSnowfall(filename)
+
--- q3.py
+++ q3.py
@@ -1,1 +1,30 @@
+import sys
+file_name = sys.argv[1]
 def wordCount(t):
+    """Read the file and return a dictionary of unique words as keys, and the lines number they appear as a value"""
+    input_file = open(t, "r")
+    file_contents = input_file.read()
+    word_list = file_contents.split()
+    line_number = 0
+    unique_Words = set(word_list)
+
+    dict_words = dict.fromkeys(unique_Words)
+
+    """For every line in the file, create a set of unique words, and append the line number to the dictionary value if the word is found as a key"""
+    with open(t) as file_words:
+        for line in file_words:
+            line = line.strip()
+            list = line.split()
+            unique = set(list)
+            for word in unique:
+                if word in dict_words:
+                    if dict_words.get(word) == None:
+                        dict_words.update({word:[line_number]})
+                    else:
+                        dict_words[word].append(line_number)
+            line_number += 1
+    return dict_words
+print(wordCount(file_name))
+
+
+
